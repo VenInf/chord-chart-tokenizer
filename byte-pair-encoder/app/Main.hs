afaf9f2f3fd271cda10d6768454e24ccd3d4c6e6
George Gerasev <george30032002@gmail.com>
2024-04-23 18:40:04 +0300

    init

---
+++ byte-pair-encoder/app/Main.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# OPTIONS_GHC -fno-cse #-}
+
+module Main (main) where
+
+import Lib
+import System.Console.CmdArgs
+
+data BPE = BPE {dict_file :: FilePath
+                  ,vocab_size :: Int
+                  }
+                  deriving (Show, Data, Typeable)
+
+sample = BPE{dict_file = def
+            ,vocab_size = 0}
+
+main = print =<< cmdArgs sample
